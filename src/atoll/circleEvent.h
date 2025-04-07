2f1ed2249ee089d62cee542336f3caa1508861bc
GreyKeenan <156843326+GreyKeenan@users.noreply.github.com>
2025-04-07 17:03:04 -0500

    siteEvent & circleEvent tidying
    	went back through the logic, trying to find bug
    	Didn't find it, but did tidy them up in the process.

--- src/atoll/circleEvent.h
+++ src/atoll/circleEvent.h
@@ -11,68 +11,51 @@
 
 /*heap*/ struct gu_echo *atoll_circleEvent(struct atoll_diagram diagram[static 1], struct atoll_coast coast[static 1], struct atoll_circle event)
 {
-
-	/*
-
-	join converging edges
-		init a new edge at event.center
-		set as a vertex for both of the converging edges (4 hedges)
-	cut old arc & one of the old breaks[] edges indices
-		update the remaining circle event indices
-	create new edge and overwrite the remaining old edge with it
-	set 1 vertex of the new edge to the same as the converged-at vertex
-
-	check for new circle events with the arcs adjacent to the changed-edge?
-
-	*/
-
-	if (event.arc == 0 || event.arc >= coast->foci_length) {
-		return gu_echo_new(event.arc, "event.arc shouldn't be the first/last item ever");
-	}
-
 	int e = 0;
 
+	// create the vertex at the convergence point
+	const unsigned int newvertex = diagram->vertices_length;
+	const struct atoll_point vertex_struct = {
+		.x = event.center.x,
+		.y = event.center.y
+	};
+	e = gu_unstable_intlist_push(&diagram->vertices, &diagram->vertices_length, &diagram->vertices_allocation, &vertex_struct);
+	if (e) return gu_echo_new(e, "unable to push new vertex");
 
+	// create the new edge
+	const unsigned int newedge = diagram->hedges_length;
+	e = atoll_diagram_newedge(diagram, coast->foci[event.arc - 1], coast->foci[event.arc + 1]);
+	if (e) return gu_echo_new(e, "unable to create new half edges");
+
+	// set the convergence as a vertex for the new edge & the converging edges
+	e = atoll_edge_giveVertex(diagram->hedges, newedge, newvertex);
+	if (e) return gu_echo_new(e, "failed to set vertex for the new edge");
+	e = atoll_edge_giveVertex(diagram->hedges, coast->breaks[event.arc - 1], newvertex);
+	if (e) return gu_echo_new(e, "failed to set vertex for the left bp");
+	e = atoll_edge_giveVertex(diagram->hedges, coast->breaks[event.arc], newvertex);
+	if (e) return gu_echo_new(e, "failed to set vertex for the right bp");
+
+	// remove the invalidated circle events
 	atoll_coast_removeCirclesWithArc(&coast->circles, &coast->circles_length, event.arc - 1);
 	atoll_coast_removeCirclesWithArc(&coast->circles, &coast->circles_length, event.arc    );
 	atoll_coast_removeCirclesWithArc(&coast->circles, &coast->circles_length, event.arc + 1);
 
-
-	unsigned int vertidx = diagram->vertices_length;
-	const struct atoll_point vtx = {
-		.x = event.center.x,
-		.y = event.center.y
-	};
-	e = gu_unstable_intlist_push(&diagram->vertices, &diagram->vertices_length, &diagram->vertices_allocation, &vtx);
-	if (e) return gu_echo_new(e, "unable to push new vertex");
-
-	e = atoll_edge_giveVertex(diagram->hedges, coast->breaks[event.arc - 1], vertidx);
-	if (e) return gu_echo_new(e, "unable to give vertex (1/2)");
-	e = atoll_edge_giveVertex(diagram->hedges, coast->breaks[event.arc], vertidx);
-	if (e) return gu_echo_new(e, "unable to give vertex (2/2)");
-
-
+	// remove the focus & 1 breakpoint
 	gu_intlist_cut(&coast->foci, &coast->foci_length, event.arc, 1);
 	gu_intlist_cut(&coast->breaks, &coast->breaks_length, event.arc - 1, 1);
+	// overwrite the remaining breakpoint w/ the new edge
+	coast->breaks[event.arc - 1] = newedge;
 
+	// update the circle-event-indices since the foci-list was changed
 	atoll_coast_updateCircles(coast->circles, coast->circles_length, event.arc, -1);
+		// TODO: maybe this is breaking somehow?
 
-	e = atoll_diagram_newedge(diagram, coast->foci[event.arc], coast->foci[event.arc - 1]);
-	if (e) return gu_echo_new(e, "failed to create new edge");
-
-	e = atoll_edge_giveVertex(diagram->hedges, diagram->hedges_length - 2, vertidx);
-	if (e) return gu_echo_new(e, "unable to give vertex to the new edge");
-	coast->breaks[event.arc - 1] = diagram->hedges_length - 2;
-
-	
-
+	// identify new circle events, checking the neighbors of the removed arc
 	e = atoll_circle_identify(coast, diagram->sites, event.arc, event.center.y + event.radius);
 	if (e) return gu_echo_new(e, "failed when identifying circle events (1/2)");
-
 	e = atoll_circle_identify(coast, diagram->sites, event.arc - 1, event.center.y + event.radius);
 	if (e) return gu_echo_new(e, "failed when identifying circle events (2/2)");
-
-
+	
 	return NULL;
 }
 
--- src/atoll/siteEvent.h
+++ src/atoll/siteEvent.h
@@ -20,81 +20,56 @@ static inline /*heap*/ struct gu_echo *atoll_siteEvent(
 {
 	int e = 0;
 
-	// ==========
-	/*
-	These cases must be handled by atoll_coast_prime() before calling atoll_siteEvent().
-
-	CASE: coast->foci_length == 0
-
-	CASE: First `n` sites (`n > 1`) are horizontally aligned.
-
-	So, in other words, this function assumes:
-
-	* the beachline has at least 1 arc already
-	* not all foci on the beachline have the same `y`
-
-	*/
-	// ==========
-
-
+	// find the index of the parabola to bisect
 	unsigned int at = 0;
 	e = atoll_coast_arcAtX(coast->foci, coast->foci_length, diagram->sites, diagram->sites[event].y, diagram->sites[event].x, &at);
 	if (e) return gu_echo_new(e, "failed to identify parabola to bisect");
 
-
-	if ( !(at == 0 || at == coast->foci_length - 1) ) {
-		atoll_coast_removeCirclesWithArc(&coast->circles, &coast->circles_length, at);
-	}
-
-
-	// add the new breakpoint-edge
-
+	// create the new edge
+	const unsigned int newedge = diagram->hedges_length;
 	e = atoll_diagram_newedge(diagram, at, event);
 	if (e) return gu_echo_new(e, "failed to create the half edges");
 
-	unsigned int dummy = diagram->hedges_length - 2;
-
-	if (at == coast->foci_length - 1) {
-		e = gu_unstable_intlist_push(&coast->breaks, &coast->breaks_length, &coast->breaks_allocation, &dummy);
-		if (e) return gu_echo_new(e, "failed to push breakpoint-edge 1/2");
-
-		e = gu_unstable_intlist_push(&coast->breaks, &coast->breaks_length, &coast->breaks_allocation, &dummy);
-		if (e) return gu_echo_new(e, "failed to push breakpoint-edge 2/2");
-	} else {
-		e = gu_intlist_pry(&coast->breaks, &coast->breaks_length, &coast->breaks_allocation, at, 2);
-		if (e) return gu_echo_new(e, "failed to pry for breakpoint-edges");
-
-		coast->breaks[at] = dummy;
-		coast->breaks[at + 1] = dummy;
-	}
+	// remove the invalidated circle events
+	atoll_coast_removeCirclesWithArc(&coast->circles, &coast->circles_length, at);
 
 
 	// TODO: because using pry() & pry() uses fit(), are re-allocating for each pry after a point.
 	// should update in gu_list to use some sort of growfit()
 
-	// add the new foci
 
+	// add the new foci
 	e = gu_intlist_pry(&coast->foci, &coast->foci_length, &coast->foci_allocation, at, 2);
 	if (e) return gu_echo_new(e, "failed to pry for foci");
-
-	// assumes foci[at] is unchanged after prying
-	coast->foci[at + 1] = event;
+	coast->foci[at + 1] = event; // assumes foci[at] is unchanged after prying
 	
 	// update the circle indices-to-foci with the offset from newly added foci
 	atoll_coast_updateCircles(coast->circles, coast->circles_length, at, 2);
 
-	// ==========
+	// add the new breakpoint-to-edges
+	if (at == coast->foci_length - 1) {
+		e = gu_unstable_intlist_push(&coast->breaks, &coast->breaks_length, &coast->breaks_allocation, &newedge);
+		if (e) return gu_echo_new(e, "failed to push breakpoint-edge 1/2");
+
+		e = gu_unstable_intlist_push(&coast->breaks, &coast->breaks_length, &coast->breaks_allocation, &newedge);
+		if (e) return gu_echo_new(e, "failed to push breakpoint-edge 2/2");
+	} else {
+		e = gu_intlist_pry(&coast->breaks, &coast->breaks_length, &coast->breaks_allocation, at, 2);
+		if (e) return gu_echo_new(e, "failed to pry for breakpoint-edges");
+
+		coast->breaks[at] = newedge; // remember that (at) is the index below the new parabola
+		coast->breaks[at + 1] = newedge;
+	}
+
 
 	// find & add circle events
 	e = atoll_circle_identify(coast, diagram->sites, at, diagram->sites[event].y);
 	if (e) return gu_echo_new(e, "failed when identifying circle events (1/2)");
-
 	e = atoll_circle_identify(coast, diagram->sites, at + 2, diagram->sites[event].y);
 	if (e) return gu_echo_new(e, "failed when identifying circle events (2/2)");
 
-
 	return NULL;
-}	
+}
 
 
 #endif
