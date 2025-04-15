9c13eee32cd16663b94d64c0cdfc8455b54ecaaa
GreyKeenan <156843326+GreyKeenan@users.noreply.github.com>
2025-04-14 22:44:22 -0500

    gumetry_point_average()

--- src/gumetry/point.h
+++ src/gumetry/point.h
@@ -3,4 +3,15 @@
 
 struct gumetry_point { double x, y; };
 
+static inline struct gumetry_point gumetry_point_average(struct gumetry_point * /*nonull*/ points, unsigned int count)
+{
+	double x = 0;
+	double y = 0;
+	for (unsigned int i = 0; i < count; ++i) {
+		x += points[i].x;
+		y += points[i].y;
+	}
+	return (struct gumetry_point) { x/count, y/count };
+}
+
 #endif
